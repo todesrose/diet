1cc9baab178cf6d8c7c15acbfad56cb67ab84987
todesrose <67089126+todesrose@users.noreply.github.com>
2020-06-18 08:20:01 +0300

    Add files via upload

---
+++ database/migrations/2020_06_15_000000_create_meals_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMealsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meals', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+			$table->string('name', 50);
+			$table->text('recept');
+			$table->integer('carbohydrates')->unsigned()->nullable();
+			$table->integer('fats')->unsigned()->nullable();
+			$table->integer('protein')->unsigned()->nullable();
+			$table->integer('calories')->unsigned()->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meal');
+    }
+}
---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::truncate();
+        User::create(array('name' => 'Administrator',
+                           'email' => 'admin@diet.test', 
+                           'password' => bcrypt('secret'),
+                           'role' => 1));
+    }
+}
